d1fe66881e02020cb67031d60cfbc2ffacf12718
Maxim Suslov <MSuslov@luxoft.com>
2018-08-06 10:11:43 +0200

    bin: add TODO for du_sort.sh

--- bin/du_sort.sh
+++ bin/du_sort.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # du_sort
 # du_sort src/* | head -n 2
+# TODO: folder with spaces aren't work
 du -sh $@ | perl -e 'sub h{%h=(K=>10,M=>20,G=>30);($n,$u)=shift=~/([0-9.]+)(\D)/; return $n*2**$h{$u}}print sort{h($b)<=>h($a)}<>;'
 
