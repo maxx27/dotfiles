680507ad81f71e686d7c82eca03f605d0d451789
Maxim Suslov <MSuslov@luxoft.com>
2018-09-26 10:54:31 +0200

    tmux: update config and build

--- bin/tmux_build_from_source.sh
+++ bin/tmux_build_from_source.sh
@@ -3,15 +3,16 @@ set -x -e
 
 # Steps to build and install tmux from source on Ubuntu.
 VERSION=2.7
-sudo apt-get -y remove tmux
+sudo apt-get -y remove tmux || sudo dpkg -r tmux || true
 sudo apt-get -y install wget tar libevent-dev libncurses-dev checkinstall
 wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
+[ -e tmux-${VERSION} ] && sudo rm -rf tmux-${VERSION}
 tar xf tmux-${VERSION}.tar.gz
 rm -f tmux-${VERSION}.tar.gz
 cd tmux-${VERSION}
 ./configure --prefix=/usr/local
 make -j $(nproc)
-sudo checkinstall -y --pkgname tmux --pkgversion ${VERSION}.
+sudo checkinstall -y --pkgname tmux --pkgversion ${VERSION}
 # sudo make install
 # cd -
 # sudo rm -rf /usr/local/src/tmux-*
