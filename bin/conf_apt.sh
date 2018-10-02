e0c76dc96891595ef252599dda55672678f5d729
Maxim Suslov <MSuslov@luxoft.com>
2018-10-02 06:53:54 +0300

    bin: structure optimization

--- bin/conf_apt.sh
+++ bin/conf_apt.sh
@@ -1,5 +1,10 @@
 checkinstall
 
+fonts-powerline
+https://github.com/powerline/fonts
+
+xfonts-terminus
+
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get install google-chrome-stable
