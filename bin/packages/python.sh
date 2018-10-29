13d85976bf021142550cd846c8f2fb3e1fcdec9f
Maxim Suslov <MSuslov@luxoft.com>
2018-10-29 10:28:05 +0300

    bin: fix python modules

--- bin/packages/python.sh
+++ bin/packages/python.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PACKAGES=pip pep8 neovim flask waitress
+PACKAGES=pip pep8 neovim flask waitress psutil requests ppretty
 for package in $PACKAGES; do
     python -m pip install --upgrade $package
 done
