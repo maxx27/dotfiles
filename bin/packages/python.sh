2fd9adb91458c94e3c305b2c11b5b0369a6353aa
Maxim Suslov <MSuslov@luxoft.com>
2018-11-26 06:12:49 +0300

    python: update module list

--- bin/packages/python.sh
+++ bin/packages/python.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-# pep8 -> pycodestyle
-PACKAGES=pip pycodestyle neovim flask waitress psutil requests ppretty pytest pyreadline
+PACKAGES=$(cat <<EOF | awk '{print $1;}'
+    pip         - to update pip itself
+    pycodestyle - replacement for pep8
+    pyreadline  - to use readline library
+    ipython     - replacement for idle
+    ppretty     - to print pretty structures
+    pytest
+    requests    - web crawling tools
+    flask
+    waitress    - application server for flask
+    psutil
+    neovim
+EOF
+)
+
 for package in $PACKAGES; do
     python -m pip install --upgrade $package
 done
