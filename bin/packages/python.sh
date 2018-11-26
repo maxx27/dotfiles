fc428a1bb0b63d5729b891dad78a88405de69147
Maxim Suslov <MSuslov@luxoft.com>
2018-11-26 05:42:40 +0300

    python: update module list

--- bin/packages/python.sh
+++ bin/packages/python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # pep8 -> pycodestyle
-PACKAGES=pip pycodestyle neovim flask waitress psutil requests ppretty pytest
+PACKAGES=pip pycodestyle neovim flask waitress psutil requests ppretty pytest pyreadline
 for package in $PACKAGES; do
     python -m pip install --upgrade $package
 done
