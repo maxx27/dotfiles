5f17778895f0f7636cce2c0db3140f8716dd2a7b
Maxim Suslov <MSuslov@luxoft.com>
2019-02-06 11:07:44 +0100

    bin: update build of ripgrep.sh

--- bin/build/ripgrep.sh
+++ bin/build/ripgrep.sh
@@ -7,6 +7,14 @@ set -x -e
 sudo apt-get -y install cargo
 [ ! -e ripgrep ] && git clone https://github.com/BurntSushi/ripgrep
 cd ripgrep
+
+# workaroung for the error:
+#   attempted ssh-agent authentication, but none of the usernames `git` succeeded
+# https://github.com/rust-lang/cargo/issues/3381
+# eval `ssh-agent -s`
+# ssh-add
+# cargo ...
+
 cargo build --release
 cargo test --all
 
