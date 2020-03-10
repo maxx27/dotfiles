e162d386338d6dd0848a424c400f887ad33f5808
Maxim Suslov <MSuslov@luxoft.com>
2020-03-10 16:16:47 +0300

    bin update

---
+++ bin/detach_users.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+# remove all detached users
+# https://superuser.com/questions/358835/force-logout-a-user
+for pid in $(
+    for ptsn in $(w | grep **user_name** | grep pts | awk '{print $2}'); do
+        ps -dN | grep "$ptsn " | awk '{print $1}' ;
+    done
+    ); do
+    kill -9 $pid;
+done
---
+++ bin/luxoft/cert/readme.txt
@@ -0,0 +1,1 @@
+Перенесите на другие машины папку /usr/share/ca-certificates/luxoft/ и выполните sudo dpkg-reconfigure ca-certificates. Не забудьте отметить новые сертификаты в мастере импорта.
