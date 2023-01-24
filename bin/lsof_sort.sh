2547e89e8b3ba085fe8e3a430cdae31893f75968
Maxim Suslov <suslov.mn@gmail.com>
2023-01-24 14:37:24 +0300

    bin: add lsof_sort.sh

---
+++ bin/lsof_sort.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+lsof $* \
+| grep REG \
+| grep -v "stat: No such file or directory" \
+| grep -v DEL \
+| awk '{if ($NF=="(deleted)") {x=3;y=1} else {x=2;y=0}; {print $(NF-x) "  " $(NF-y) } }'  \
+| sort -n -u -r  \
+| numfmt  --field=1 --to=iec
