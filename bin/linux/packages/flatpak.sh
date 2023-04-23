6becbe3c9a050b694abf7a93d3af307cdc3469f3
Maxim Suslov <suslov.mn@gmail.com>
2023-04-23 08:09:41 +0300

    bin: update flatpak packages

---
+++ bin/linux/packages/flatpak.sh
@@ -0,0 +1,9 @@
+$ flatpak list --app --columns=name,application
+Name                          Application ID
+GitFiend                      com.gitfiend.GitFiend
+Gittyup                       com.github.Murmele.Gittyup
+OBS Studio                    com.obsproject.Studio
+WhatsApp Desktop              io.github.mimbrero.WhatsAppDesktop
+Obsidian                      md.obsidian.Obsidian
+MuseScore                     org.musescore.MuseScore
+Telegram Desktop              org.telegram.desktop
